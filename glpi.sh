81553205c9e3214a7ec81ee906e9e91aa64f7e45
lucasg-luz <lucas.greco359@gmail.com>
2025-12-23 10:33:39 -0300

    Update glpi.sh

--- glpi.sh
+++ glpi.sh
@@ -100,7 +100,7 @@ EOF
 echo "⬇ Downloading latest GLPI..."
 
 cd /tmp
-curl -fsSL https://github.com/glpi-project/glpi/releases/latest/download/glpi.tgz -o glpi.tgz
+curl -fsSL https://github.com/glpi-project/glpi/releases/download/11.0.4/glpi-11.0.4.tgz -o glpi.tgz
 
 echo "Extracting GLPI..."
 tar -xzf glpi.tgz
